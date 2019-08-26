7ded43ce226446a2844efa4180e1733220b2d172
hennd <rafael.andrade@ulbra.inf.br>
2019-08-25 21:42:02 -0300

    ddsdsd

---
+++ app/src/main/java/com/example/cheapest/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.cheapest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtLitrao;
+    EditText edtLatao;
+    EditText edtLatinha;
+    Button btnCalcula;
+    TextView txtResposta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtLatao = findViewById(R.id.edtValor500);
+        edtLitrao = findViewById(R.id.edtValor1000);
+        edtLatinha = findViewById(R.id.edtValor273);
+        txtResposta = findViewById(R.id.txtResposta);
+        btnCalcula = findViewById(R.id.btnCalcular);
+
+
+        btnCalcula.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String retorno = calcular(Double.valueOf(edtLitrao.getText().toString()), Double.valueOf(edtLatao.getText().toString()), Double.valueOf(edtLatinha.getText().toString()));
+                txtResposta.setText(retorno);
+            }
+        });
+
+
+    }
+
+    public String calcular(Double litrao, Double latao, Double latinha) {
+
+        int controle=0;
+        String camponegativo="";
+        String valornulo="nulo";
+        if (litrao <= 0) {
+            controle=1;
+            camponegativo+=" Litrao";
+        }
+
+        if (latao <= 0 ) {
+            controle=1;
+            camponegativo+=", Latao";
+        }
+        if (latinha <= 0 ) {
+            controle=1;
+            camponegativo+=", Latinha";
+        }
+
+        if (controle == 0) {
+
+            double divLitrao = litrao / 1000;
+            double divLatao = latao / 473;
+            double divLatinha = latinha / 350;
+            double talvez = divLitrao;
+            String vai = "nada";
+            double referencia = 0;
+
+            if (talvez < divLatao) {
+                vai = "Vai de Litrão";
+            } else {
+                talvez = divLatao;
+                vai = "Vai de Latao";
+                referencia = divLatao;
+            }
+            if (talvez > divLatinha) {
+                vai = "Vai de Latinha";
+                referencia = divLatinha;
+            }
+
+            referencia = talvez;
+
+            if ((divLatao == divLatinha) & (referencia == divLatao)) {
+                vai = "Pode ir de Litrao ou Latinha";
+            } else if ((divLatao == divLitrao) & (referencia == divLatao)) {
+                vai = "Pode ir de Latao ou Litrao";
+            } else if ((divLatinha == divLitrao) & (referencia == divLatinha)) {
+                vai = "Pode ir de Latinha ou Latao";
+            }
+
+            if ((divLatao == divLatinha) & (divLatao == divLitrao))
+                vai = "Tanto faz";
+            return vai;
+        }
+        else
+        return "voce digitou um valor invalido nos campos:"+camponegativo;
+
+
+
+    }
+}
